b46dbdb4477cf424c492e3c4b55bf47cad826980
F2253474 Daniel Braga Moura <danielbraga@bb.com.br>
2020-07-04 17:39:58 -0300

    ajustes notificacao

--- app/src/main/java/com/example/watermind/MainActivity.java
+++ app/src/main/java/com/example/watermind/MainActivity.java
@@ -1,16 +1,21 @@
 package com.example.watermind;
 
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.ContextCompat;
-
+import android.app.AlarmManager;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TimePicker;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.Calendar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,86 +28,167 @@ public class MainActivity extends AppCompatActivity {
     private Button btnNotify;
     private EditText editMinutes;
 
-    private int interval;
-    private int hour;
-    private int minute;
-
+    private SharedPreferences storage;
     private boolean activated;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        storage = getSharedPreferences("storage", Context.MODE_PRIVATE);
 
         timePicker = findViewById(R.id.time_picker);
         btnNotify = findViewById(R.id.button_notify);
         editMinutes = findViewById(R.id.edit_number_interval);
 
-        final SharedPreferences storage = getSharedPreferences("storage", Context.MODE_PRIVATE);
-        activated = storage.getBoolean(KEY_INTERVAL, false);
+        activated = storage.getBoolean(KEY_NOTIFY, false);
 
-        if (activated){
-            btnNotify.setText(R.string.pause);
-            btnNotify.setBackgroundColor(ContextCompat.getColor(MainActivity.this, android.R.color.darker_gray));
-            editMinutes.setText(String.valueOf(storage.getInt(KEY_INTERVAL, 0)));
-            timePicker.setCurrentHour(storage.getInt(KEY_HOUR, timePicker.getCurrentHour()));
-            timePicker.setCurrentMinute(storage.getInt(KEY_MINUTE, timePicker.getCurrentMinute()));
-
-        } else {
-            btnNotify.setText(R.string.notify);
-            btnNotify.setBackgroundColor(ContextCompat.getColor(MainActivity.this, android.R.color.holo_blue_light));
-
-        }
+        setupUI(activated, storage);
 
         timePicker.setIs24HourView(true);
 
-        btnNotify.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
+        btnNotify.setOnClickListener(notifyListener);
+    }
 
-                if (!activated) {
-                    String sInterval = editMinutes.getText().toString();
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Teste", "onRestart");
+    }
 
-                    if (sInterval.isEmpty()) {
-                        Toast.makeText(MainActivity.this, getString(R.string.textoIntervalo), Toast.LENGTH_LONG).show();
-                        return;
-                    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i("Teste", "onResume");
+    }
 
-                    interval = Integer.parseInt(sInterval);
-                    hour = timePicker.getCurrentHour();
-                    minute = timePicker.getCurrentMinute();
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Teste", "onPause");
+    }
 
-                    btnNotify.setText(R.string.pause);
-                    btnNotify.setBackgroundColor(ContextCompat.getColor(MainActivity.this, android.R.color.darker_gray));
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Teste", "onStart");
+    }
 
-                    SharedPreferences.Editor edit = storage.edit();
-                    edit.putBoolean(KEY_NOTIFY, true);
-                    edit.putInt(KEY_INTERVAL, interval);
-                    edit.putInt(KEY_HOUR, hour);
-                    edit.putInt(KEY_MINUTE, minute);
-                    edit.apply();
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Teste", "onStop");
+    }
 
-                    activated = true;
-                } else {
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Teste", "onDestroy");
+    }
 
-                    btnNotify.setText(R.string.notify);
-                    btnNotify.setBackgroundColor(ContextCompat.getColor(MainActivity.this, android.R.color.holo_blue_light));
+    private void alert(int resId) {
+        Toast.makeText(MainActivity.this, resId, Toast.LENGTH_LONG).show();
+    }
 
-                    SharedPreferences.Editor edit = storage.edit();
-                    edit.putBoolean(KEY_NOTIFY, false);
-                    edit.remove(KEY_INTERVAL);
-                    edit.remove(KEY_HOUR);
-                    edit.remove(KEY_MINUTE);
-                    edit.apply();
+    private boolean intervalIsValid() {
+        String sInterval = editMinutes.getText().toString();
 
-                    activated = false;
-                }
+        if (sInterval.isEmpty()) {
+            alert(R.string.validation);
+            return false;
+        }
 
+        if (sInterval.equals("0")) {
+            alert(R.string.zero_value);
+            return false;
+        }
 
-            }
-        });
+        return true;
+    }
 
+    private  void setupUI(boolean activated, SharedPreferences storage) {
+        if (activated) {
+            btnNotify.setText(R.string.pause);
+            btnNotify.setBackgroundResource(R.drawable.bg_button_background);
+            editMinutes.setText(String.valueOf(storage.getInt(KEY_INTERVAL, 0)));
+            timePicker.setCurrentHour(storage.getInt(KEY_HOUR, timePicker.getCurrentHour()));
+            timePicker.setCurrentMinute(storage.getInt(KEY_MINUTE, timePicker.getCurrentMinute()));
+        } else {
+            btnNotify.setText(R.string.notify);
+            btnNotify.setBackgroundResource(R.drawable.bg_button_background_accent);
+        }
+    }
 
+    private void updateStorage(boolean added, int interval, int hour, int minute) {
+        SharedPreferences.Editor edit = storage.edit();
+        edit.putBoolean(KEY_NOTIFY, added);
+
+        if (added) {
+            edit.putInt(KEY_INTERVAL, interval);
+            edit.putInt(KEY_HOUR, hour);
+            edit.putInt(KEY_MINUTE, minute);
+        } else {
+            edit.remove(KEY_INTERVAL);
+            edit.remove(KEY_HOUR);
+            edit.remove(KEY_MINUTE);
+        }
+
+        edit.apply();
+    }
+
+    private void setupNotification(boolean added, int interval, int hour, int minute) {
+        Intent notificationIntent = new Intent(MainActivity.this, NotificationPublisher.class);
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context.ALARM_SERVICE);
+
+        if (added) {
+            Calendar calendar = Calendar.getInstance();
+            calendar.set(Calendar.HOUR_OF_DAY, hour);
+            calendar.set(Calendar.MINUTE, minute);
+            calendar.set(Calendar.SECOND, 0);
+
+            notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION_ID, 1);
+            notificationIntent.putExtra(NotificationPublisher.KEY_NOTIFICATION, "Hora de beber água");
+
+            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0,
+                    notificationIntent, PendingIntent.FLAG_CANCEL_CURRENT);
+
+            alarmManager.setRepeating(AlarmManager.RTC_WAKEUP, calendar.getTimeInMillis(),
+                    interval * 100, broadcast);
+        } else {
+            PendingIntent broadcast = PendingIntent.getBroadcast(MainActivity.this, 0,
+                    notificationIntent, 0);
+
+            alarmManager.cancel(broadcast);
+        }
 
     }
+
+    private View.OnClickListener notifyListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            if (!activated) {
+
+                if (!intervalIsValid()) return;
+
+                int interval = Integer.parseInt(editMinutes.getText().toString());
+                int hour = timePicker.getCurrentHour();
+                int minute = timePicker.getCurrentMinute();
+
+                updateStorage(true, interval, hour, minute);
+                setupUI(true, storage);
+                setupNotification(true, interval, hour, minute);
+                alert(R.string.notified);
+
+                activated = true;
+            } else {
+                updateStorage(false, 0, 0, 0);
+                setupUI(false, storage);
+                setupNotification(false, 0,0,0);
+                alert(R.string.notified_pause);
+
+                activated = false;
+            }
+        }
+    };
+
 }
